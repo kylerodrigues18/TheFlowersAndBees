eeb73e5304f0c134c3e14ab661922fd6faf8087b
Kyle Rodrigues <kylerodrigues18@gmail.com>
2019-12-13 12:57:01 -0600

    Implemented Flowers and they now show up into the interface, also moved images

--- src/code/ChooseImage.java
+++ src/code/ChooseImage.java
@@ -1,19 +1,8 @@
 package code;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 public class ChooseImage {
 
-    private Path currentRelativePath = Paths.get("");
-    private String currentPath = currentRelativePath.toAbsolutePath().toString();
-    private String imageFolderLocation = "\\src\\code\\images\\";
-
-    private String fileLocation = currentPath + imageFolderLocation;
-
-    private String getFilePath() {
-        return fileLocation;
-    }
+    private String fileLocation = "\\images\\";
 
     public String getFlowerFile() {
         int fileExtension = ((int) (Math.random() * 6)) + 1;
